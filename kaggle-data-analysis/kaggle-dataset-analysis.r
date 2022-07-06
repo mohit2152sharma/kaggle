fb3ba2702042738b561b6bef96d1d1454b9b3772
Mohit Sharma <mohitlakshya@gmail.com>
2022-07-06 18:59:40 +0530

    Kaggle Notebook | Kaggle Dataset Analysis | Version 1

---
+++ kaggle-data-analysis/kaggle-dataset-analysis.r
@@ -0,0 +1,76 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "2e10fdf8",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "papermill": {
+     "duration": 0.001975,
+     "end_time": "2022-07-06T13:29:30.363709",
+     "exception": false,
+     "start_time": "2022-07-06T13:29:30.361734",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# Kaggle DataSet Analysis"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "86231c93",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001034,
+     "end_time": "2022-07-06T13:29:30.365918",
+     "exception": false,
+     "start_time": "2022-07-06T13:29:30.364884",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "This notebook contains the following information:\n",
+    "\n",
+    "1. The most popular dataset of the day\n",
+    "2. The most popular dataset of the week\n",
+    "3. The most popular dataset of the month\n",
+    "4. The most popular dataset of the year\n",
+    "5. What makes the dataset popular?\n",
+    "    1. Which topic are the hottest in datasets"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.57641,
+   "end_time": "2022-07-06T13:29:30.487724",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2022-07-06T13:29:26.911314",
+   "version": "2.3.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
